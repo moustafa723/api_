ccee1348d02b21d9ecb1200b34a96a5207a96e9d
elgazzar <moustafaelgazzar20@gmail.com>
2026-07-11 19:13:14 +0300

    Debug loader

--- modal_app.py
+++ modal_app.py
@@ -2,6 +2,7 @@ import modal
 
 image = (
     modal.Image.debian_slim()
+    .add_local_dir(".", remote_path="/root")
     .pip_install_from_requirements("requirements.txt")
 )
 
@@ -11,5 +12,5 @@ app = modal.App("surgenius-api")
 @app.function(image=image)
 @modal.asgi_app()
 def fastapi_app():
-    from app import app as fastapi_app
-    return fastapi_app
+    from app import app
+    return app
